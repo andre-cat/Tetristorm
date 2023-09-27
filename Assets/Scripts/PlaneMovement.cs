06639c9000ed5f51c0f7e59ee724b56ef121c506
sebachoh <sebaruiz01@gmail.com>
2023-09-27 09:42:04 -0500

    AddedScript for planeMovement
    AddedScript for Movement and includes the SineWaveMovement, I also added the CinemachineCamera but it's still to configure it bc we haven't decided with position it would be.

---
+++ Assets/Scripts/PlaneMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+
+    [Header("For the basic movement")]
+    [Space(10)]
+    [SerializeField] private float speed = 5.0f;
+
+    [Header("For the Sine Movement")]
+    [Space(5)]
+    [SerializeField] private float amplitude = 2.0f;
+    [SerializeField] private float frequency = 0.5f;
+    private float sinCenterY;
+
+    public bool isInverted = false;
+
+
+    void Start()
+    {
+        sinCenterY = transform.position.y;
+    }
+
+    void FixedUpdate()
+    {
+        BasicMovement();
+        SineWaveMovement();
+        
+    }
+
+    void SineWaveMovement()
+    {
+        Vector3 pos = transform.position;
+
+        float sin = Mathf.Sin(pos.x * frequency) * amplitude;
+        if (isInverted)
+        {
+            sin *= -1;
+        }
+        pos.y = sinCenterY + sin;
+
+        transform.position = pos;
+    }
+
+    void BasicMovement()
+    {
+        Vector2 pos = transform.position;
+
+        pos.x -= speed * Time.fixedDeltaTime;
+
+
+
+        transform.position = pos;
+    }
+}
