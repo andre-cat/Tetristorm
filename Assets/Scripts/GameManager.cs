be4c80abb12ac45e32d521e0409f3c7df2023954
camilolc <kmilo3025@hotmail.com>
2023-09-29 15:02:05 -0500

    update canvas and game feeling

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,11 +9,20 @@ public class GameManager : MonoBehaviour
     public AnimationClip crossfadeAnimClip;
     public int sceneNumber;
     AudioManager audioManager;
+
+    public bool isPaused = false;
+    public GameObject pausePanel;
+
     // Start is called before the first frame update
     void Start()
     {
         crossFadeAnim = GameObject.Find("PanelForCrossfade").GetComponent<Animator>();
         audioManager = FindObjectOfType<AudioManager>();
+
+        if (pausePanel)
+        {
+            pausePanel.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -25,6 +34,9 @@ public class GameManager : MonoBehaviour
         }
 
         ChangeMusicForScene();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            TogglePausePanel();
     }
 
     public IEnumerator ChangeScene()
@@ -33,7 +45,7 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(crossfadeAnimClip.length+1f); // used 1 seconds instead of crossfadeAnimClip.length bc it wasn't working properly idk why. (I know the length is 1 seconds bc the inspector)
         SceneManager.LoadScene(sceneNumber+1);
 
-    }
+    }   
 
     public void Wrapper()
     {
@@ -55,4 +67,30 @@ public class GameManager : MonoBehaviour
             audioManager.PlayGameOverMusic();
         }
     }
+
+    public void TogglePausePanel()
+    {
+        Debug.Log("Paused");
+        isPaused = !isPaused;
+        if (pausePanel)
+        {
+            pausePanel.SetActive(isPaused);
+            Time.timeScale = isPaused ? 0 : 1;
+        }
+    }
+
+    public void GoToMainMenu()
+    {
+        StartCoroutine("MainMenu");
+    }
+
+    public IEnumerator MainMenu()
+    {
+        crossFadeAnim.SetBool("SceneCompleted", true);
+        Time.timeScale = 1;
+        yield return new WaitForSeconds(crossfadeAnimClip.length + 0.5f); // used 1 seconds instead of crossfadeAnimClip.length bc it wasn't working properly idk why. (I know the length is 1 seconds bc the inspector)
+        SceneManager.LoadScene(0);
+
+    }
+
 }
