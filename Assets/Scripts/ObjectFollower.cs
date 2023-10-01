93355263751c24d1163a7ade5bdf055d23c63aca
andre-cat <andrecat-@outlook.com>
2023-10-01 04:04:45 -0500

    Scripts: for following target and change color by weather

---
+++ Assets/Scripts/ObjectFollower.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollower : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = target.position - transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 position = target.position;
+
+        transform.position = position - offset;
+    }
+}
